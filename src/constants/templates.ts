2670a814e49804ae2d4272c393b05b673c15b36f
Podolnyi Aleksandr <colourmatik811@gmail.com>
2025-04-25 10:12:04 +0300

    created template gallery component for home

---
+++ src/constants/templates.ts
@@ -0,0 +1,37 @@
+export const templates = [
+    {
+        id: "blank", 
+        label: "Пустой  документ", 
+        imageUrl: "/blank-document.svg"
+    },
+    {
+        id: "software-proposal", 
+        label: "Предложение по разработке программного обеспечения", 
+        imageUrl: "/software-proposal.svg"
+    },  
+    {
+        id: "project-proposal", 
+        label: "Проектное редложение ", 
+        imageUrl: "/project-proposal.svg"
+    },    
+    {
+        id: "business-letter", 
+        label: "Бизнес письмо", 
+        imageUrl: "/business-letter.svg"
+    },   
+    {
+        id: "resume", 
+        label: "Резюме", 
+        imageUrl: "/resume.svg"
+    },   
+    {
+        id: "cover-letter", 
+        label: "Сопроводительное письмо", 
+        imageUrl: "/cover-letter.svg"
+    },  
+    {
+        id: "letter", 
+        label: "Письмо", 
+        imageUrl: "/letter.svg"
+    },         
+];
