edacab2ac0d6f5054021414a3950b1a13173f493
Podolnyi Aleksandr <colourmatik811@gmail.com>
2025-05-06 11:12:31 +0300

    populated user avatars and visibility for current users in room

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -15,7 +15,7 @@ import {
     MenubarSubTrigger,
     MenubarTrigger ,
 } from "@/components/ui/menubar";
-
+import { Avatars } from "./avatars";
 import { DocumentInput } from "./document-input";
 import { BoldIcon, FileIcon, FileJsonIcon, FilePenIcon, FilePlusIcon, FileTextIcon, GlobeIcon, ItalicIcon, PrinterIcon, Redo2Icon, RemoveFormattingIcon, StrikethroughIcon, TextIcon, TrashIcon, UnderlineIcon, Undo2Icon } from "lucide-react";
 import { BsFilePdf } from "react-icons/bs";
@@ -208,6 +208,7 @@ export const Navbar = () => {
                 </div>
             </div>
             <div className="flex gap-3 items-center pl-6">
+                        <Avatars/>
                         <OrganizationSwitcher
                         afterCreateOrganizationUrl="/"
                         afterLeaveOrganizationUrl="/"
