ab877c18cb7e46f76d12882e5d93008caac034e6
Podolnyi Aleksandr <colourmatik811@gmail.com>
2025-05-11 18:08:41 +0300

    small fixes

--- src/app/documents/[documentId]/loading.tsx
+++ src/app/documents/[documentId]/loading.tsx
@@ -1,7 +1,7 @@
 import { FullscreenLoader } from "@/components/fullscreen-loader";
 
 const LoadingPage = () => {
-  return <FullscreenLoader label="Document loading..." />
+  return <FullscreenLoader label="Загрузка документа..." />
 }
  
 export default LoadingPage;
