173c14283d12ba4ced4667368a8444af999d537c
Podolnyi Aleksandr <colourmatik811@gmail.com>
2025-05-11 18:09:54 +0300

    menu items now clickable

--- src/app/documents/[documentId]/navbar.tsx
+++ src/app/documents/[documentId]/navbar.tsx
@@ -23,13 +23,33 @@ import { useEditorStore } from "@/store/use-editor-store";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 import { Inbox } from "./inbox";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { useMutation } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { RenameDialog } from "@/components/rename-dialog";
+import { RemoveDialog } from "@/components/remove-dialog";
 
 interface NavbarProps {
     data: Doc<"documents">;
 };
 
 export const Navbar = ({data}: NavbarProps) => {
+    const router = useRouter();
     const {editor} = useEditorStore();
+    const mutation = useMutation(api.documents.create)
+
+    const onNewDocument = () => {
+        mutation({
+            title:"Безымянный документ",
+            initialContent:""
+        })
+        .catch(() => toast.error("Что-то пошло не так..."))
+        .then((id) => {
+            toast.success("Документ создан")
+            router.push(`/documents/${id}`)
+        });
+    }
 
     const insertTable = ({ rows, cols }: {rows:number, cols:number}) => {
     editor
@@ -118,19 +138,30 @@ export const Navbar = ({data}: NavbarProps) => {
                                             </MenubarItem>
                                         </MenubarSubContent>
                                     </MenubarSub>
-                                    <MenubarItem>
+                                    <MenubarItem onClick={onNewDocument}>
                                         <FilePlusIcon className="size-4 mr-2"/>
-                                        Создать
+                                        Новый документ
                                     </MenubarItem>
                                     <MenubarSeparator/>
-                                        <MenubarItem>
+                                    <RenameDialog documentId={data._id} initialTitle={data.title}>
+                                        <MenubarItem
+                                        onClick={(e) => e.stopPropagation()}
+                                        onSelect={(e) => e.preventDefault()}
+                                        >
                                             <FilePenIcon className="size-4 mr-2"/>
                                             Переименовать
                                         </MenubarItem>
-                                        <MenubarItem>
-                                            <TrashIcon className="size-4 mr-2"/>
+                                    </RenameDialog>
+                                    <RemoveDialog documentId={data._id}> 
+                                        <MenubarItem
+                                            onClick={(e) => e.stopPropagation()}
+                                            onSelect={(e) => e.preventDefault()}
+                                        >
+                                            <TrashIcon className="size-4 mr-2" />
                                             Удалить
                                         </MenubarItem>
+                                        </RemoveDialog>
+
                                     <MenubarSeparator/>
                                     <MenubarItem onClick={() => window.print()}>
                                             <PrinterIcon className="size-4 mr-2"/>
