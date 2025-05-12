2b3ada9df1ea321d09bc55a2eb17e0daa1413dd0
Podolnyi Aleksandr <colourmatik811@gmail.com>
2025-05-12 10:19:31 +0300

    added some temporal templates

--- src/app/(home)/templates-gallery.tsx
+++ src/app/(home)/templates-gallery.tsx
@@ -47,7 +47,7 @@ export const TemplatesGallery = () => {
                                     >
                                         <button
                                          disabled={isCreating}
-                                         onClick={() => onTemplateClick(template.label, "")}
+                                         onClick={() => onTemplateClick(template.label, template.initialContent)}
                                          style={{
                                             backgroundImage: `url(${template.imageUrl})`,
                                             backgroundSize: "cover",
